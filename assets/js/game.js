5a5de0f64ef15aaeef5aa2417a29cbdbc72de12d
mark manalo <m.e.manalo77@gmail.com>
2023-04-17 13:30:00 -0400

    execute variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt ("What is your robot's name?");
+console.log(playerName);
+console.log(10+10)
+function fight() {
+    window.alert("the fight has begun!");
+   
+}
+
+//fight ();
