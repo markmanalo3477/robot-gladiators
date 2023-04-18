fb934c026ccdff6b537de318aa58738c6caa0b61
mark manalo <m.e.manalo77@gmail.com>
2023-04-18 01:12:02 -0400

    initialized robots fighting

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,36 @@
-var playerName = window.prompt ("What is your robot's name?");
-console.log(playerName);
-console.log(10+10)
-function fight() {
-    window.alert("the fight has begun!");
-   
+var playerName = window.prompt(" What's your robot's name");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = " Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    //Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+    
+
+// Subtract the value of ' playerAttack ' from the value of ' enemyHealth '
+enemyHealth = enemyHealth - playerAttack;
+
+//Log a resulting message to the console so we know that it worked.
+console.log( 
+    playerName + " attacked " + enemyName + "." + enemyName+ " now has " + enemyHealth + " healthRemaining ");
+
+// execute function
+};
+//  check enemy's health
+if ( enemyHealth <= 0)  {
+    window.alert(enemyName + " has died !");
+
+}
+else { 
+    window.alert(enemyName + "still has " + enemyHealth + " health left ");
 }
 
-//fight ();
+
+fight();
+ 
